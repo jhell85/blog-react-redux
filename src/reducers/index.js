073b6dc757a3c5da32b601473c4209366709f912
jhell85 <jhell85@gmail.com>
2020-09-23 17:59:01 -0700

    fixed reducers to not return undefined

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,6 @@
 import { combineReducers } from "redux";
 import postsReducer from "./postsReducer";
-export default (state = [], action) => {
-  switch (action.type) {
-    case "FETCH_POSTS":
-      return action.payload;
-    default:
-      return state;
-  }
-};
+
+export default combineReducers({
+  posts: postsReducer,
+});
